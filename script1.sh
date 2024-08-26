1eea9a74354da9e756fa1566e27a47b7f73645dc
Aymogul <aymogul22@gmail.com>
2024-08-26 15:39:01 -0700

    script

--- script1.sh
+++ script1.sh
@@ -17,7 +17,7 @@ sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# Add the repository to Apt sources:
+# Add the repository to Apt sources: this is for documentation 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
